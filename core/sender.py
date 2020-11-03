057f9431c2eab7ea4070ff7633c8697c9204ec1a
Ivan Nikolsky <enty8080@gmail.com>
2020-11-03 10:38:21 +0300

    sender initialized.

--- core/sender.py
+++ core/sender.py
@@ -1,10 +1,16 @@
+import os
+
+from core.badges import badges
+
 class sender:
-    def __init__(self):
-        self.init = 1
+    def __init__(self, unicorn_handler):
+        self.unicorn = unicorn_handler
+        self.badges = badges()
         
     def send_command(self, command, cmd_data):
         template = []
         template.append(command)
         template.append(cmd_data)
         
-        return template
+        self.unicorn.send(template)
+        return self.unicorn.recv().split().decode("UTF-8", "ignore")
