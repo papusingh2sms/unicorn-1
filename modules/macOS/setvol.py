b2923ce8c50ebf7cd68fa737d1a600dbc25fa3f4
Ivan Nikolsky <enty8080@gmail.com>
2020-11-03 19:40:03 +0300

    Create setvol.py

---
+++ modules/macOS/setvol.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from core.badges import badges
+from core.sender import sender
+
+class UnicornModule:
+    def __init__(self, unicorn_handler):
+        self.sender = sender(unicorn_handler)
+        self.badges = badges()
+        
+        self.name = "setvol"
+        self.description = "Set output sound volume."
+        self.usage = "Usage: setvol [0-100]"
+        self.type = "settings"
+        self.args = 2
+        
+    def run(self, cmd_data):
+        self.sender.send_command("shell", "osascript -e 'set volume output volume "+cmd_data+"'", False)
