3445eed46a71f18d4402b9d3b02143d23cc93749
Ivan Nikolsky <enty8080@gmail.com>
2020-11-06 14:13:01 +0300

    Update screenshot.py

--- modules/macOS/screenshot.py
+++ modules/macOS/screenshot.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 
 from core.badges import badges
+from core.fsmanip import fsmanip
 
 class UnicornModule:
     def __init__(self, unicorn):
         self.unicorn = unicorn
         self.badges = badges()
+        self.fsmanip = fsmanip()
 
         self.name = "screenshot"
         self.description = "Take screenshot."
-        self.usage = "Usage: screenshot <output_path>"
+        self.usage = "Usage: screenshot <local_path>"
         self.type = "managing"
         self.args = 2
 
     def run(self, cmd_data):
         print(self.badges.G + "Taking screenshot...")
         image = self.unicorn.send_command(self.name, cmd_data, True, False)
+        exists, path_type = self.fsmanip.exists_directory(cmd_data)
+        if exists:
+            if path_type != "file":
+                if cmd_data[-1] == "/":
+                    cmd_data = cmd_data + "screenshot.png"
+                else:
+                    cmd_data = cmd_data + "/screenshot.png"
         f = open(cmd_data, "wb")
         print(self.badges.G + "Saving to " + cmd_data + "...")
         f.write(image)
